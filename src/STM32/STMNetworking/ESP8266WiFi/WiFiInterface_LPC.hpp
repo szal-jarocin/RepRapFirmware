0ee5e75c564f7edacfb79d0e2a364936a51a9930
Andy Shaw <andy-git@gloomy-place.com>
2020-09-04 15:21:15 +0100

    Stop DMA transfer when CS goes high

--- src/STM32/STMNetworking/ESP8266WiFi/WiFiInterface_LPC.hpp
+++ src/STM32/STMNetworking/ESP8266WiFi/WiFiInterface_LPC.hpp
@@ -4,6 +4,7 @@
 
 //ESP connected to SSP0
 static HardwareSPI *spiDevice;
+static volatile bool dmaActive;
 
 //maintain compatibility with RRF code
 void spi_disable(SSPChannel sspChannel) noexcept
@@ -18,44 +19,65 @@ static inline void DisableSpi() noexcept
 
 static inline void spi_dma_disable() noexcept
 {
-    // For RRF compatibility
+    spiDevice->stopTransfer();
 }
 
 static bool spi_dma_check_rx_complete() noexcept
 {
-    //DMA Interrupt will notifiy when transfer is complete, just return true here
-    spiDevice->checkComplete();
-    return true;
+    if (!transferPending)
+    {
+        // If CS goes high before the DMA transfer completes we need to abort
+        // the transfer.
+        if (dmaActive)
+            spiDevice->stopTransfer();
+        dmaActive = false;
+        return true;
+    }
+    return false;
 }
 
 
 // SPI completion handler
 void ESP_SPI_HANDLER(HardwareSPI *spiDevice) noexcept
 {
+    dmaActive = false;
 	wifiInterface->SpiInterrupt();
 }
 
-static void spi_slave_dma_setup(uint32_t dataOutSize, uint32_t dataInSize) noexcept
+void WiFiInterface::spi_slave_dma_setup(uint32_t dataOutSize, uint32_t dataInSize) noexcept
 {
     //Find the largest transfer size
     const uint32_t dsize = MAX(dataOutSize + sizeof(MessageHeaderSamToEsp), dataInSize + sizeof(MessageHeaderEspToSam));
     // clear any previous transaction
     spiDevice->flushRx();
-    spiDevice->startTransfer((const uint8_t *)&bufferOut, (uint8_t *)&bufferIn, dsize, ESP_SPI_HANDLER);
+    dmaActive = true;
+    spiDevice->startTransfer((const uint8_t *)bufferOut, (uint8_t *)bufferIn, dsize, ESP_SPI_HANDLER);
 }
 
 void WiFiInterface::SpiInterrupt() noexcept
 {
-    digitalWrite(SamTfrReadyPin, LOW);
+    if (transferPending)
+    {
+        digitalWrite(SamTfrReadyPin, LOW);
 
-    transferPending = false;
-    TaskBase::GiveFromISR(espWaitingTask);
+        transferPending = false;
+        TaskBase::GiveFromISR(espWaitingTask);
+    }
+}
+
+static void SpiCSInterrupt(CallbackParameter) noexcept
+{
+    // Called when CS goes high. If the DMA trasnfer has not yet completed
+    // we force it to end early.
+    if (transferPending && digitalRead(PB_12) != 0)
+        wifiInterface->SpiInterrupt();    
 }
 
 
 // Set up the SPI system
 void WiFiInterface::SetupSpi() noexcept
 {
+    attachInterrupt(PB_12, SpiCSInterrupt, INTERRUPT_MODE_RISING, nullptr);
     spiDevice = &HardwareSPI::SSP2;
     spiDevice->configureDevice(SPI_MODE_SLAVE, 8, (uint8_t)SPI_MODE_1, 100000000, true);
 }
