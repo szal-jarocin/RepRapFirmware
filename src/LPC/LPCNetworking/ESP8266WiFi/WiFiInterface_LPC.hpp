4990838ed03235b30a89d1d333fdf7203177d691
Andy Shaw <andy-git@gloomy-place.com>
2020-09-06 21:13:39 +0100

    Abort SPI DMA when CS goes high

--- src/LPC/LPCNetworking/ESP8266WiFi/WiFiInterface_LPC.hpp
+++ src/LPC/LPCNetworking/ESP8266WiFi/WiFiInterface_LPC.hpp
@@ -6,6 +6,7 @@
 
 //ESP connected to SSP0
 static HardwareSPI *spiDevice;
+static volatile bool dmaActive;
 
 //maintain compatibility with RRF code
 void spi_disable(SSPChannel sspChannel) noexcept
@@ -20,19 +21,28 @@ static inline void DisableSpi() noexcept
 
 static inline void spi_dma_disable() noexcept
 {
-    // For RRF compatibility
+    spiDevice->stopTransfer();
 }
 
 static bool spi_dma_check_rx_complete() noexcept
 {
-    //DMA Interrupt will notifiy when transfer is complete, just return true here
-    return true;
+    if (!transferPending)
+    {
+        // If CS goes high before the DMA transfer completes we need to abort
+        // the transfer.
+        if (dmaActive)
+            spiDevice->stopTransfer();
+        dmaActive = false;
+        return true;
+    }
+    return false;
 }
 
 
 // SPI completion handler
 void ESP_SPI_HANDLER(HardwareSPI *spiDevice) noexcept
 {
+    dmaActive = false;
 	wifiInterface->SpiInterrupt();
 }
 
@@ -41,22 +51,36 @@ void WiFiInterface::spi_slave_dma_setup(uint32_t dataOutSize, uint32_t dataInSiz
     //Find the largest transfer size
     const uint32_t dsize = MAX(dataOutSize + sizeof(MessageHeaderSamToEsp), dataInSize + sizeof(MessageHeaderEspToSam));
     // clear any previous transaction
+    dmaActive = true;
     spiDevice->flushRx();
     spiDevice->startTransfer((const uint8_t *)bufferOut, (uint8_t *)bufferIn, dsize, ESP_SPI_HANDLER);
 }
 
 void WiFiInterface::SpiInterrupt() noexcept
 {
-    digitalWrite(SamTfrReadyPin, LOW);
+    if (transferPending)
+    {
+        digitalWrite(SamTfrReadyPin, LOW);
 
-    transferPending = false;
-    TaskBase::GiveFromISR(espWaitingTask);
+        transferPending = false;
+        TaskBase::GiveFromISR(espWaitingTask);
+    }
+}
+
+static void SpiCSInterrupt(CallbackParameter) noexcept
+{
+    // Called when CS goes high. If the DMA trasnfer has not yet completed
+    // we force it to end early.
+    if (transferPending && digitalRead(SPI0_SSEL) != 0)
+        wifiInterface->SpiInterrupt();    
 }
 
 
 // Set up the SPI system
 void WiFiInterface::SetupSpi() noexcept
 {
+    attachInterrupt(SPI0_SSEL, SpiCSInterrupt, INTERRUPT_MODE_RISING, nullptr);
+
     spiDevice = &HardwareSPI::SSP0;
     //In Slave mode, the SSP clock rate provided by the master must not exceed 1/12 of the
     //SSP peripheral clock (which is set to PCLK/1 above), therefore to cater for LPC1768
