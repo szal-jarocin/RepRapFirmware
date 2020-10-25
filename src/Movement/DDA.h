0c76ae6ae2f19983c69b12aae36a8c1c504ed042
Andy Shaw <andy-git@gloomy-place.com>
2020-10-25 14:23:17 +0000

    Add in STM config

--- src/Movement/DDA.h
+++ src/Movement/DDA.h
@@ -139,7 +139,11 @@ public:
 	static constexpr uint32_t MinCalcIntervalDelta = (40 * StepTimer::StepClockRate)/1000000; 		// the smallest sensible interval between calculations (40us) in step timer clocks
 	static constexpr uint32_t MinCalcIntervalCartesian = (40 * StepTimer::StepClockRate)/1000000;	// same as delta for now, but could be lower
 	static constexpr uint32_t HiccupTime = 20;														// how long we hiccup for
-#elif defined(__LPC17xx__)
+#elif __LPC17xx__
+    static constexpr uint32_t MinCalcIntervalDelta = (40 * StepTimer::StepClockRate)/1000000;		// the smallest sensible interval between calculations (40us) in step timer clocks
+    static constexpr uint32_t MinCalcIntervalCartesian = (40 * StepTimer::StepClockRate)/1000000;	// same as delta for now, but could be lower
+	static constexpr uint32_t HiccupTime = 20;														// how long we hiccup for
+#elif STM32F4
     static constexpr uint32_t MinCalcIntervalDelta = (40 * StepTimer::StepClockRate)/1000000;		// the smallest sensible interval between calculations (40us) in step timer clocks
     static constexpr uint32_t MinCalcIntervalCartesian = (40 * StepTimer::StepClockRate)/1000000;	// same as delta for now, but could be lower
 	static constexpr uint32_t HiccupTime = 20;														// how long we hiccup for
