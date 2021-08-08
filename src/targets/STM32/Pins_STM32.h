d95a958e46e9fddb14cf4e050a33cfe6bafbe321
Andy Shaw <andy-git@gloomy-place.com>
2021-08-08 13:18:51 +0100

    Enable async moves to be in line with duet

--- src/targets/STM32/Pins_STM32.h
+++ src/targets/STM32/Pins_STM32.h
@@ -58,7 +58,7 @@
 
 #define SUPPORT_LED_STRIPS               1
 #define SUPPORT_BITBANG_NEOPIXEL         1
-#define SUPPORT_ASYNC_MOVES		         0
+#define SUPPORT_ASYNC_MOVES		         1
 #define ALLOCATE_DEFAULT_PORTS           0
 #define TRACK_OBJECT_NAMES		         1
 
