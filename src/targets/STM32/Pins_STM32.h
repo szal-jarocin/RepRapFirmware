238a30adf5c4c4698ddaa694cfe9e38db84ecd17
Andy Shaw <andy-git@gloomy-place.com>
2021-08-21 08:48:48 +0100

    Enable accelerometers in SBC mode, raise axis limit

--- src/targets/STM32/Pins_STM32.h
+++ src/targets/STM32/Pins_STM32.h
@@ -92,7 +92,7 @@
     #define HAS_MASS_STORAGE             0
     #define HAS_LINUX_INTERFACE          1
     #define SUPPORT_TELNET               1
-    #define SUPPORT_ACCELEROMETERS       0
+    #define SUPPORT_ACCELEROMETERS       1
     #define HAS_WRITER_TASK              0
 
     #define BOARD_NAME          "STM32F4 SBC"
@@ -162,7 +162,7 @@ constexpr size_t MaxGpInPorts = 10;
 constexpr size_t MaxGpOutPorts = 10;
 
 constexpr size_t MinAxes = 3;                        // The minimum and default number of axes
-constexpr size_t MaxAxes = 6;                        // The maximum number of movement axes in the machine, usually just X, Y and Z, <= DRIVES
+constexpr size_t MaxAxes = 10;                       // The maximum number of movement axes in the machine, usually just X, Y and Z, <= DRIVES
 constexpr size_t MaxDriversPerAxis = 4;              // The maximum number of stepper drivers assigned to one axis
 
 constexpr size_t MaxExtruders = 8;                   // The maximum number of extruders
